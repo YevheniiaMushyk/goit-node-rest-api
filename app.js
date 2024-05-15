d2ffe999c747af2b49f724c34b870dccd276bf9e
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-05-15 19:12:48 +0300

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
-	res.status(404).json({ message: "Route not found" });
+	res.status(404).json({ message: "Route not found :(" });
 });
 
 app.use((err, req, res, next) => {
@@ -21,6 +21,6 @@ app.use((err, req, res, next) => {
 	res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-	console.log("Server is running. Use our API on port: 3000");
+app.listen(8080, () => {
+	console.log("Server is running. Use our API on port: http://localhost:8080");
 });
